d11e67f8841ef3357b5d0e7b6bd3075bb222a5dc
IINova1 <142625192+IINova1@users.noreply.github.com>
2025-08-26 18:05:10 -0400

    Update models.py

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -6,8 +6,12 @@ class Categoria(models.Model):
     def __str__(self):
         return self.nombre
     
-
-
+class zona(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
+    def __str__(self):
+        return self.nombre
+    
 class Dispositivo(models.Model):
     nombre = models.CharField(max_length=100)
     consumo = models.IntegerField()
@@ -15,3 +19,20 @@ class Dispositivo(models.Model):
 
     def __str__(self):
         return self.nombre
+    
+class medicion(models.Model):
+    dispositivo = models.ForeignKey(Dispositivo, on_delete=models.CASCADE)
+    fecha_hora = models.DateTimeField(auto_now_add=True)
+    consumo = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.dispositivo.nombre} - {self.fecha_hora} - {self.consumo}W"
+    
+class alerta(models.Model):
+    dispositivo = models.ForeignKey(Dispositivo, on_delete=models.CASCADE)
+    mensaje = models.TextField()
+    fecha_hora = models.DateTimeField(auto_now_add=True)
+    leida = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.dispositivo.nombre} - {self.mensaje[:20]} - {'Leida' if self.leida else 'No Leida'}"

92caed09925dd20d81f6fe34df6191a9ca9f998a
IINova1 <142625192+IINova1@users.noreply.github.com>
2025-08-26 17:52:13 -0400

    update

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+    Descripcion = models.TextField(blank=True, null=True)
+    def __str__(self):
+        return self.nombre
+    
+
+
+class Dispositivo(models.Model):
+    nombre = models.CharField(max_length=100)
+    consumo = models.IntegerField()
+    estado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nombre
